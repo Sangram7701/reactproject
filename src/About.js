2cff95ef2ca1b612722998ddd33c70a7ae97a5f4
Sangram chauhan <sangramchauhan8957@gmail.com>
2025-07-06 23:30:48 +0530

    changed code

---
+++ src/About.js
@@ -0,0 +1,14 @@
+function Aboutpage(){
+    return <h1>this is heading</h1>
+}
+export default Aboutpage  
+export function About1(){
+    return <p>This is name export function</p>
+} 
+export function Abc(){
+    return <h2>This is heading two</h2>
+}
+
+export function Abcd1(){
+    return <h4>This is name export1</h4>
+}
